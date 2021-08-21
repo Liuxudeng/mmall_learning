f129315b056e9a7914a1ff4fefc89e164e151ef5
Liuxudeng <1833746919@qq.com>
2021-08-21 17:32:15 +0800

    commit_message

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -1,2 +1,10 @@
-package com.mmall.controller.backend;public class CategoryManageController {
+package com.mmall.controller.backend;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/manage/category")
+public class CategoryManageController {
 }
